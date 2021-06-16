7d980d76451ba18e53917829e668487387093bd9
NMAqilah <81207460+NMAqilah@users.noreply.github.com>
2021-06-16 18:39:03 +0800

    update change

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
   echo "Hi World!";
   echo "<br>";
     echo "My name is Aqilah and i am 23 years old";
-    echo "I study in Multimedia Universsity";
+    echo "<br>";
+    echo "I study in Multimedia University";
+    echo "<br>";
     echo "Bachelor in Data Communication and Networking";
+    echo "<br>";
     echo "Have a nice day!!";
  
 ?>
